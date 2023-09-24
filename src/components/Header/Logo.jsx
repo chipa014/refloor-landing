538ed8b2a082a47802d456fe5d6891660daa1e19
chipa014 <alextheracoonthesecond@gmail.com>
2023-09-24 21:37:42 +0700

    Image display bugfixing

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -1,13 +1,11 @@
+import Logo from "../../assets/img/logo.jpg";
+
 import styles from "./Logo.module.scss";
 
 const Logo = function () {
   return (
     <a href="#" className={styles.container}>
-      <img
-        src={require("/src/assets/img/logo.jpg")}
-        alt="Домашняя страница"
-        className={styles.logo}
-      />
+      <img src={Logo} alt="Домашняя страница" className={styles.logo} />
     </a>
   );
 };
